884d04e25bbf51fc2e19a71a71c342bcb314d144
Joao <marketting@letscode.com.br>
2024-08-20 16:42:00 -0300

    resolvido o problema do metodo getAndRemove da listaDuplamenteEncadeada

---
+++ src/main/java/listaduplaencadeada/ListaEncadeadaDupla.java
@@ -0,0 +1,163 @@
+package listaduplaencadeada;
+
+import listaencadeadasimples.ListaEncadeada;
+
+public class ListaEncadeadaDupla<Dado> {
+    private No head = null;
+    private No tail = null ;
+    private No current;
+    private int tamanho = 0;
+
+    public class No{
+        private Dado elemento;
+        private No next = null;
+        private No previus = null;
+
+        public No(Dado elemento){
+            this.elemento = elemento;
+        }
+
+        public String toString(){
+            return ""+elemento;
+        }
+    }
+
+
+
+    public void add(Dado elemento){
+        No newNo = new No(elemento);
+
+        current = tail;
+        tail = newNo;
+
+        if(current == null)  head = tail;
+        else{
+            current.next = newNo;
+            tail.previus = current;
+            tamanho++;
+        }
+    }
+
+    public void addinicio(Dado elemento){
+        No newNo = new No(elemento);
+
+        current = head;
+        head = newNo;
+
+        if(current == null) tail = head;
+        else{
+            head.next = current;
+            current.previus = head;
+            tamanho++;
+        }
+    }
+
+    public Dado get(Dado elemento){
+        current = head;
+
+        while(current != null && current.elemento != elemento ){
+            current = current.next;
+        }
+
+        if(current == null) return null;
+        else{
+            return current.elemento;
+        }
+    }
+
+    public Dado getAndRemove(Dado elemento){
+        No currentTail = tail;
+        if(head == null) return null;
+
+        current = head;
+
+            /*while (current != null && current.elemento != elemento) {
+                current = current.next;
+            }
+
+            if(current == head){
+                head = head.next;
+                tamanho --;
+                return current.elemento;
+            } else if(currentTail != null && current == currentTail){ // erro na hora de encontrar um jeito de achar a cauda
+                current.previus.next = null;
+                tamanho --;
+                return current.elemento;
+            }
+            else{
+                assert current != null;
+                current.next.previus = current.previus;
+                current.previus.next = current.next;
+                tamanho--;
+                return current.elemento;
+            }*/
+        /*    while (current != null && current.elemento != elemento) current = current.next;
+            if (current != null) {
+                if (current == head) {
+                    head = head.next;
+                    head.previus = null;
+                }
+                else if (current == tail){
+                    tail = tail.previus;
+                    tail.next = null;
+                }
+                else {
+                    current.previus.next = current.next;
+                    current.next.previus = current.previus;
+                }
+
+                tamanho--;
+                return current.elemento;
+            }
+            else return null; */
+        while (current != null && !current.elemento.equals(elemento)) {
+            current = current.next;
+        }
+
+        if (current != null) {
+            if (current == head) {
+                head = head.next;
+                if (head != null) {
+                    head.previus = null;
+                } else {
+                    tail = null; // Caso onde a lista fica vazia
+                }
+            } else if (current == tail) {
+                tail = tail.previus;
+                if (tail != null) {
+                    tail.next = null;
+                } /* else {
+                    head = null; // Caso onde a lista fica vazia
+                }*/
+            } else {
+                current.previus.next = current.next;
+                current.next.previus = current.previus;
+            }
+
+            tamanho--;
+            return current.elemento;
+        } else {
+            return null;
+        }
+
+
+
+
+    }
+
+
+
+    public String toString(){
+        current = head;
+        String str = " ";
+
+        while(current != null){
+            str = str +current.elemento+" ";
+            current = current.next;
+        }
+
+        return str;
+    }
+
+
+}
---
+++ src/test/java/ListaEncadedaDuplaTeste.java
@@ -0,0 +1,64 @@
+import listaduplaencadeada.ListaEncadeadaDupla;
+import org.junit.jupiter.api.Test;
+
+public class ListaEncadedaDuplaTeste {
+    @Test
+    public void ListaEncadeadaTeste(){
+        ListaEncadeadaDupla lista = new ListaEncadeadaDupla<>();
+
+        lista.add('2');
+        lista.add('3');
+        lista.add(4);
+        lista.add(5);
+        lista.addinicio(99);
+        lista.add(78);
+
+        System.out.println("Lista completa antes de ser removida: "+lista);
+
+        System.out.println(" ");
+        System.out.println("Ordem dos numeros a serem removidos");
+
+        Object teste = lista.getAndRemove('2');
+        Object teste2 = lista.getAndRemove('3');
+        Object teste3 = lista.getAndRemove(99);
+        Object teste4 = lista.getAndRemove(4);
+        Object teste5 = lista.getAndRemove(78);
+        Object teste6 = lista.getAndRemove(5);
+
+
+
+        //ordem dos numeros que foram tirados
+        System.out.println(teste);
+        System.out.println(teste2);
+        System.out.println(teste3);
+        System.out.println(teste4);
+        System.out.println(teste5);
+        System.out.println(teste6);
+
+        System.out.println(" ");
+
+        System.out.println("Essa são os valores da lista depois de removidos: "+lista);
+
+
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.println("Adcionando outros elementos na lista");
+
+        lista.add(5);
+        lista.add(365);
+        lista.add(364);
+
+        System.out.println(" ");
+        System.out.println("Nova lista adcionada"+lista);
+
+
+
+
+
+
+
+
+
+    }
+}
